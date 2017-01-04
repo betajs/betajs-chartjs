dbed0c5e01e5fb7c8007144a6defa3998f65157d
Pablo I <pablo.iglesias02@gmail.com>
2017-01-04 12:00:59 -0300

    Added title and legend support

--- src/charts_base.js
+++ src/charts_base.js
@@ -7,27 +7,75 @@ Scoped.define("module:ChartJS", [
 		
 		template : "<div><canvas></canvas></div>",
 
-        _validateData: function() {
-            if (!this.get("chart-data"))
-                throw "Chart data must be set";
-            var data = this.get("chart-data");
-            if (!data.labels)
-                console.warn("You might be missing the data labels");
-            this._validateDataSets();
+        attrs: {
+		    title: false,
+            legend: true,
+            colors: [],
+            chartdata: null,
+            options: {}
 
         },
 
-        _validateDataSets: function() {
-            var data = this.get("chart-data");
-            if (!data.datasets)
+        _init: function() {
+		    if (this.get("title"))
+		        this.__addTitle(this.get("title"));
+		    if (this.get("legend") !== true)
+                this.__addLegend(this.get("legend"));
+
+		    this.__validateData();
+        },
+
+        __validateData: function() {
+            if (!this.get("chartdata"))
+                throw "Chart chartdata must be set";
+            var chartdata = this.get("chartdata");
+            if (!chartdata.labels)
+                console.warn("You might be missing the data labels");
+            this.__validateDataSets();
+
+        },
+
+        __validateDataSets: function() {
+            var chartdata = this.get("chartdata");
+            if (!chartdata.datasets)
                 console.warn("You might be missing the datasets");
 
-            data.datasets.forEach(function(current, index, main) {
+            chartdata.datasets.forEach(function(current, index, main) {
                 if (!current.label)
                     console.warn("A dataset doesn't have a label");
-                if (data.labels && (!current.data || current.data.length !== data.labels.length))
+                if (chartdata.labels && (!current.data || current.data.length !== chartdata.labels.length))
                     console.warn("The amount of data points doesn't match chart labels");
             });
+        },
+
+        __addTitle: function (value) {
+            var title = "";
+            if (typeof value !== "object") {
+                title = {
+                    display: true,
+                    text: value
+                };
+            } else {
+                title = value;
+            }
+            this.__addOption("title", title);
+        },
+
+        __addLegend: function (value) {
+		    var legend = "";
+		    if (typeof value === "object") {
+                legend = value;
+            } else {
+                if (!value)
+                    legend = {
+                        display: false
+                    };
+            }
+            this.__addOption("legend", legend);
+        },
+
+        __addOption: function (key, value) {
+            this.setProp("options." + key, value);
         }
 	
 	});
--- src/charts_line.js
+++ src/charts_line.js
@@ -6,15 +6,14 @@ Scoped.define("module:ChartJS.Line", [
 	var Cls = ChartsElem.extend({scoped: scoped}, {
 		
 		initial : {
-
             create : function() {
-                this._validateData();
+                this._init();
                 var element = this.element().find("canvas").get(0);
 
                 this.graph = new Chart(element, {
                     type: "line",
-                    data: this.get("chart-data"),
-                    options: this.get("chart-options")
+                    data: this.get("chartdata"),
+                    options: this.get("options")
                 });
             }
 		}

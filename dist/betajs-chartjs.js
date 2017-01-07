938a359f5a5f0bc64fd8fe46642e0a22562a85db
Oliver Friedmann <oliverfriedmann@Olivers-MacBook-Pro.local>
2017-01-07 15:50:41 -0500

    Update

--- dist/betajs-chartjs.js
+++ dist/betajs-chartjs.js
@@ -1,5 +1,5 @@
 /*!
-betajs-chartjs - v1.0.5 - 2016-12-07
+betajs-chartjs - v1.0.5 - 2017-01-07
 Copyright (c) Pablo Iglesias
 Apache-2.0 Software License.
 */
@@ -693,7 +693,7 @@ Public.exports();
 	return Public;
 }).call(this);
 /*!
-betajs-chartjs - v1.0.5 - 2016-12-07
+betajs-chartjs - v1.0.5 - 2017-01-07
 Copyright (c) Pablo Iglesias
 Apache-2.0 Software License.
 */
@@ -706,155 +706,441 @@ Scoped.binding('dynamics', 'global:BetaJS.Dynamics');
 Scoped.binding('jquery', 'global:jQuery');
 Scoped.define("module:", function () {
 	return {
-    "guid": "ae5c3c39-efda-4c9a-a52f-d46fd494c9e0",
-    "version": "<%= revisioncount %>.1481127298464"
+    "guid": "3f11db99-8d84-486b-845c-ce2280ed4446",
+    "version": "10.1483822151894"
 };
 });
+Scoped.define("module:ChartJS.Bars", [
+    "module:ChartJS",
+    "base:Strings"
+], function (ChartsElem, Strings, scoped) {
+
+    var Cls = ChartsElem.extend({scoped: scoped}, {
+
+        initial : {
+            attrs: {
+                horizontal : false
+            },
+
+            create : function() {
+                var type = "bar";
+                if (this.get("horizontal")) {
+                    type = "horizontalBar";
+                }
+
+                this._init(type);
+                var element = this.element().find("canvas").get(0);
+
+                new Chart(element, this.get("chartobj"));
+            }
+        },
+
+        _getColors: function() {
+            if (this.get("randomcolors")) {
+                var chardatalength = this.get("chartdata")[0].data.length;
+                var colors = this.__getRandomColors(chardatalength);
+                var backgroundColors = [];
+                var lineColors = [];
+                colors.forEach(function(color, index) {
+                    var bgColor = "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", 0.2)";
+                    var lineColor = "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", 1)";
+                    backgroundColors.push(bgColor);
+                    lineColors.push(lineColor);
+                });
+
+                return {"backgroundColors" : backgroundColors, "lineColors": lineColors};
+            }
+        },
+
+        _addRandomColors: function(dataset) {
+            var colors = this._getColors();
+            if (!dataset.backgroundColor)
+                dataset.backgroundColor = colors.backgroundColors;
+            if (!dataset.borderColor) {
+                dataset.borderColor = colors.lineColors;
+                dataset.borderWidth = 1;
+            }
+
+            return dataset;
+        }
+    });
+
+    Cls.register("ba-chart-bars");
+
+    return Cls;
+
+});
+
 Scoped.define("module:ChartJS", [
     "dynamics:Dynamic",
-    "base:Strings",
-    "base:Timers"
-], function (Dynamic, Strings, Timers, scoped) {
+    "base:Strings"
+], function (Dynamic, Strings, scoped) {
+
+    return Dynamic.extend({scoped: scoped}, {
+
+        template : "<div><canvas></canvas></div>",
+
+
+        attrs: {
+            type: "",
+            title: false,
+            legend: true,
+            colors: [],
+            chartdata: null,
+            chartlabels: null,
+            customdataobj: null,
+            options: null,
+            chartobj: null,
+            colorset: null
+
+
+        },
+
+        _init: function(type) {
+            if (this.get("title"))
+                this.__addTitle(this.get("title"));
+            if (this.get("legend") !== true)
+                this.__addLegend(this.get("legend"));
+            if (this.get("customdataobj")) {
+                var custom = this.get("customdataobj");
+                if (custom.options && this.get("options"))
+                    custom.options = Object.assign(this.get("options"), custom.options);
+                this.set("chartobj", custom);
+                return true;
+            }
+
+            this.__validateData();
+
+            this.set("chartobj", {});
+
+            if (type !== undefined)
+                this.setProp("chartobj.type", type);
+            var data = {
+                labels: this.get("chartlabels"),
+                datasets: this.get("chartdata")
+            };
+            this.setProp("chartobj.options", this.get("options"));
+            this.setProp("chartobj.data", data);
+        },
+
+        __validateData: function() {
+            if (!this.get("chartdata"))
+                throw "Chart chartdata must be set";
+            var chartdata = this.get("chartdata");
+            if (!this.get("chartlabels"))
+                console.warn("You might be missing the data labels");
+            this.__validateDataSets();
+
+        },
+
+        __validateDataSets: function() {
+            var chartdata = this.get("chartdata");
+            var dyn = this;
+            chartdata.forEach(function(current, index, main) {
+                if (!current.label)
+                    console.warn("A dataset doesn't have a label");
+                if (dyn.get("chartlabels") && (!current.data || current.data.length !== dyn.get("chartlabels").length))
+                    console.warn("The amount of data points doesn't match chart labels");
+                if (dyn.get("randomcolors")) {
+                    main[index] = dyn._addRandomColors(current);
+                }
+            });
+        },
+
+        __addTitle: function (value) {
+            var title = "";
+            if (typeof value !== "object") {
+                title = {
+                    display: true,
+                    text: value
+                };
+            } else {
+                title = value;
+            }
+            this.__addOption("title", title);
+        },
+
+        __addLegend: function (value) {
+            var legend = "";
+            if (typeof value === "object") {
+                legend = value;
+            } else {
+                if (!value)
+                    legend = {
+                        display: false
+                    };
+            }
+            this.__addOption("legend", legend);
+        },
+
+        __addOption: function (key, value) {
+            if (!this.get("options"))
+                this.set("options", {});
+            this.setProp("options." + key, value);
+        },
+
+        __getRandomColors: function(amount) {
+            var colors = [];
+            for (var i = 1; i <= amount; i ++) {
+                var color = [(Math.floor(Math.random() * 256)), (Math.floor(Math.random() * 256)), (Math.floor(Math.random() * 256))];
+                colors.push(color);
+            }
+            return colors;
+        }
+    });
+
+});
+
+Scoped.define("module:ChartJS.Doughnut", [
+    "module:ChartJS",
+    "base:Strings"
+], function (ChartsElem, Strings, scoped) {
+
+    var Cls = ChartsElem.extend({scoped: scoped}, {
+
+        initial : {
+            create : function() {
+
+                this._init("doughnut");
+                var element = this.element().find("canvas").get(0);
+
+                new Chart(element, this.get("chartobj"));
+            }
+        },
+
+        _getColors: function() {
+            if (this.get("randomcolors")) {
+                var chardatalength = this.get("chartdata")[0].data.length;
+                var colors = this.__getRandomColors(chardatalength);
+                var backgroundColors = [];
+                var lineColors = [];
+                colors.forEach(function(color, index) {
+                    var bgColor = "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", 0.2)";
+                    backgroundColors.push(bgColor);
+                });
+
+                return {"backgroundColors" : backgroundColors, "lineColors": lineColors};
+            }
+        },
+
+        _addRandomColors: function(dataset) {
+            var colors = this._getColors();
+            if (!dataset.backgroundColor)
+                dataset.backgroundColor = colors.backgroundColors;
+
+            return dataset;
+        }
+    });
+
+    Cls.register("ba-chart-doughnut");
+
+    return Cls;
+
+});
+
+Scoped.define("module:ChartJS.Line", [
+    "module:ChartJS",
+    "base:Strings"
+], function (ChartsElem, Strings, scoped) {
 	
-	var Cls = Dynamic.extend({scoped: scoped}, {
-		
-		template : "<div><textarea>{{value}}</textarea></div>",
-		
-		initial : {
-			
-			attrs : {
-				language: "html",
-				value: ""
-			},
-			
-			create : function() {
-				if (this.initialContent)
-					this.set("value", this.initialContent);
-				if (this.get("trim") !== undefined)
-					this.set("value", Strings.nltrim(this.get("value")));
-				this.set("readonly", false);
-				this.on("change:readonly", function(value) {
-					this.codemirror.setOption("readOnly", value);
-				}, this);
-				if (this.get("auto-refresh") !== undefined) {
-					this.auto_destroy(new Timers.Timer({
-						delay : 100,
-						fire : function () {
-							this.call("refresh");
-						},
-						context : this
-					}));
-				}
-				var element = this.element().find("textarea").get(0);
-				var self = this;
-				var language = this.cls.languages[this.get("language")] || {
-					mode : this.get("language")
-				};
-				
-				this.codemirror = CodeMirror(
-						function(elt) {
-							element.parentNode.replaceChild(elt, element);
-						},
-						{
-							value : this.get("value"),
-							mode : language.mode,
-							tabMode : 'indent',
-							readOnly : this.get("readonly"),
-							highlightSelectionMatches : true,
-							autoCloseBrackets : true,
-							autoCloseTags : true,
-							lineNumbers : true,
-							// lineWrapping: true,
-							extraKeys : {
-								"Ctrl-Q" : function(cm) {
-									cm
-											.foldCode(
-													cm.getCursor(),
-													CodeMirror[language.fold]);
-								},
-								"Ctrl-Space" : function(cm) {
-									CodeMirror.commands.autocomplete = function(
-											cm) {
-										CodeMirror
-												.showHint(
-														cm,
-														CodeMirror[language.hint]);
-									};
-								}
-							},
-							foldGutter : true,
-							gutters : [ language.gutters,
-									"CodeMirror-linenumbers",
-									"CodeMirror-foldgutter" ],
-							lintWith : language.lint
-						});
-				this.codemirror.on("gutterClick", function(cm,
-						where) {
-					cm.foldCode(where, CodeMirror[language.fold]);
-				});
-				this.codemirror.setSize("100%", "100%");
-				this.__suppressUpdate = false;
-				this.codemirror.on("change", function() {
-					self.__suppressUpdate = true;
-					self.set("value", self.codemirror.getValue());
-					self.__suppressUpdate = false;
-				});
-				this.on("change:value", function(value) {
-					if (!this.__suppressUpdate)
-						this.codemirror.setValue(value);
-				}, this);
-				this.codemirror.setSize("100%", "100%");
-			},
+	var Cls = ChartsElem.extend({scoped: scoped}, {
 
-			functions : {
+        initial : {
+            create : function() {
+                this._init("line");
+                var element = this.element().find("canvas").get(0);
 
-				format : function() {
-					this.codemirror.autoFormatRange({
-						line : 0,
-						ch : 0
-					}, {
-						line : this.codemirror.lineCount() - 1,
-						ch : null
-					});
-					this.codemirror.setSelection({
-						line : 0,
-						ch : 0
-					});
-				},
+                new Chart(element, this.get("chartobj"));
+            }
+        },
 
-				refresh : function() {
-					if (this.codemirror)
-						this.codemirror.refresh();
-				}
+        _getColors: function() {
+            if (this.get("randomcolors")) {
+                var chardatalength = 1;
+                var colors = this.__getRandomColors(chardatalength);
+                var backgroundColor;
+                var lineColor;
+                colors.forEach(function(color, index) {
+                    var bgColor = "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", 0.2)";
+                    var lnColor = "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", 1)";
+                    backgroundColor  = bgColor;
+                    lineColor = lnColor;
+                });
 
-			}
-		}
-	
-	}, {
-		languages : {
-			html : {
-				fold : 'tagRangeFinder',
-				mode : "text/html",
-				hint : 'htmlHint'
-			},
-			javascript : {
-				fold : 'braceRangeFinder',
-				mode : "javascript",
-				hint : 'javascriptHint',
-				gutters : "CodeMirror-lint-markers",
-				lint : "javascriptValidator"
-			},
-			php : {
-				fold : 'tagRangeFinder',
-				mode : "application/x-httpd-php",
-				hint : 'htmlHint'
-			}
-		}
+                return {"backgroundColors" : backgroundColor, "lineColors": lineColor};
+            }
+        },
+
+        _addRandomColors: function(dataset) {
+            var colors = this._getColors();
+            if (!dataset.backgroundColor)
+                dataset.backgroundColor = colors.backgroundColors;
+            if (!dataset.borderColor) {
+                dataset.borderColor = colors.lineColors;
+                dataset.borderWidth = 1;
+            }
+            dataset.pointBackgroundColor = colors.backgroundColors;
+            dataset.pointBorderColor = '#fff';
+            dataset.pointHoverBackgroundColor = '#fff';
+            dataset.pointHoverBorderColor = colors.backgroundColors;
+
+            return dataset;
+        }
 	});
-	
-	Cls.register("ba-chart");
+
+	Cls.register("ba-chart-line");
 	
 	return Cls;
+
+});
+
+Scoped.define("module:ChartJS.Pie", [
+    "module:ChartJS",
+    "base:Strings"
+], function (ChartsElem, Strings, scoped) {
+
+    var Cls = ChartsElem.extend({scoped: scoped}, {
+
+        initial : {
+            create : function() {
+
+                this._init("pie");
+                var element = this.element().find("canvas").get(0);
+
+                new Chart(element, this.get("chartobj"));
+            }
+        },
+
+        _getColors: function() {
+            if (this.get("randomcolors")) {
+                var chardatalength = this.get("chartdata")[0].data.length;
+                var colors = this.__getRandomColors(chardatalength);
+                var backgroundColors = [];
+                var lineColors = [];
+                colors.forEach(function(color, index) {
+                    var bgColor = "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", 0.2)";
+                    backgroundColors.push(bgColor);
+                });
+
+                return {"backgroundColors" : backgroundColors, "lineColors": lineColors};
+            }
+        },
+
+        _addRandomColors: function(dataset) {
+            var colors = this._getColors();
+            if (!dataset.backgroundColor)
+                dataset.backgroundColor = colors.backgroundColors;
+
+            return dataset;
+        }
+    });
+
+    Cls.register("ba-chart-pie");
+
+    return Cls;
+
+});
+
+Scoped.define("module:ChartJS.Polar", [
+    "module:ChartJS",
+    "base:Strings"
+], function (ChartsElem, Strings, scoped) {
+
+    var Cls = ChartsElem.extend({scoped: scoped}, {
+
+        initial : {
+            create : function() {
+
+                this._init("polarArea");
+                var element = this.element().find("canvas").get(0);
+
+                new Chart(element, this.get("chartobj"));
+            }
+        },
+
+        _getColors: function() {
+            if (this.get("randomcolors")) {
+                var chardatalength = this.get("chartdata")[0].data.length;
+                var colors = this.__getRandomColors(chardatalength);
+                var backgroundColors = [];
+                var lineColors = [];
+                colors.forEach(function(color, index) {
+                    var bgColor = "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", 0.2)";
+                    backgroundColors.push(bgColor);
+                });
+
+                return {"backgroundColors" : backgroundColors, "lineColors": lineColors};
+            }
+        },
+
+        _addRandomColors: function(dataset) {
+            var colors = this._getColors();
+            if (!dataset.backgroundColor)
+                dataset.backgroundColor = colors.backgroundColors;
+
+            return dataset;
+        }
+    });
+
+    Cls.register("ba-chart-polar");
+
+    return Cls;
+
+});
+
+Scoped.define("module:ChartJS.Radar", [
+    "module:ChartJS",
+    "base:Strings"
+], function (ChartsElem, Strings, scoped) {
+
+    var Cls = ChartsElem.extend({scoped: scoped}, {
+
+        initial : {
+            create : function() {
+                this._init("radar");
+                var element = this.element().find("canvas").get(0);
+
+                new Chart(element, this.get("chartobj"));
+            }
+        },
+
+        _getColors: function() {
+            if (this.get("randomcolors")) {
+                var chardatalength = 1;
+                var colors = this.__getRandomColors(chardatalength);
+                var backgroundColor;
+                var lineColor;
+                colors.forEach(function(color, index) {
+                    var bgColor = "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", 0.2)";
+                    var lnColor = "rgba(" + color[0] + ", " + color[1] + ", " + color[2] + ", 1)";
+                    backgroundColor  = bgColor;
+                    lineColor = lnColor;
+                });
+
+                return {"backgroundColors" : backgroundColor, "lineColors": lineColor};
+            }
+        },
+
+        _addRandomColors: function(dataset) {
+            var colors = this._getColors();
+            if (!dataset.backgroundColor)
+                dataset.backgroundColor = colors.backgroundColors;
+            if (!dataset.borderColor) {
+                dataset.borderColor = colors.lineColors;
+                dataset.borderWidth = 1;
+            }
+            dataset.pointBackgroundColor = colors.backgroundColors;
+            dataset.pointBorderColor = '#fff';
+            dataset.pointHoverBackgroundColor = '#fff';
+            dataset.pointHoverBorderColor = colors.backgroundColors;
+
+            return dataset;
+        }
+    });
+
+    Cls.register("ba-chart-radar");
+
+    return Cls;
 
 });
 
